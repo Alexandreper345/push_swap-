534e22b51c8a355a3fa6b3e9830b9b2eefa9b50a
Alexandre da silva peroba <asilvaperoba@gmail.com>
2025-01-29 21:26:25 -0300

    radix pronto, estou conseguindo ordenar

--- functions_algorithm/algorith.c
+++ functions_algorithm/algorith.c
@@ -1,7 +1,7 @@
 #include "../push.h"
 
 
-void	bitwise(t_list *static_a, t_list *static_b)
+void	bitwise(t_list **static_a, t_list **static_b)
 {
 	int index_bit;
 	int len;
@@ -9,24 +9,25 @@ void	bitwise(t_list *static_a, t_list *static_b)
 	int	index_stack_b;
 	int	len_stack_b;
 
-	len = ft_list_size(static_a);
+	len = ft_list_size(*static_a);
 	index_bit = 0;
 	index_stack_b = 0;
-	while (index_bit <= 3)
+	while (index_bit < 3)
 	{
 		index = 0;
-		while (index <= len)
+		while (index < len)
 		{
-			if (static_a && (static_a->number >> index_bit) & 1)
-				ft_ra(&static_a);
+			if (*static_a && ((*static_a)->number >> index_bit) & 1)
+				ft_ra(static_a);
 			else
-				ft_pb(&static_a, &static_b);
+				ft_pb(static_a, static_b);
 			index++;	
 		}
-		len_stack_b = ft_list_size(static_b);
+		len_stack_b = ft_list_size(*static_b);
+		index_stack_b = 0;
 		while (index_stack_b < len_stack_b)
 		{
-			ft_pa(&static_a, &static_b);
+			ft_pa(static_a, static_b);
 			index_stack_b++;
 		}
 		index_bit++;
@@ -71,7 +72,7 @@ int main()
     ft_printf("Stack B antes da operação:\n");
     print_list(stack_b);
 
-    bitwise(stack_a, stack_b);
+    bitwise(&stack_a, &stack_b);
 
     ft_printf("\nStack A após a operação:\n");
     print_list(stack_a);
